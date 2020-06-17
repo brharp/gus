1797042388918b92102b0488c9b3a71cd3fb517c
Tanya Qureshi <tqureshi@uoguelph.ca>
2020-06-17 15:21:09 -0400

    add local and quicklinks menus

---
+++ src/components/quicklinks.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../styles/quicklinks.css"
+
+const QuickLinks = () => (
+<div id="quicklinksWrapper">	
+	<h4>Quick Links</h4>
+	<ul id="quick-links" className="d-flex flex-wrap">
+		<li><a href="https://intranet.uoguelph.ca"><i className="icon-intranet"></i>Intranet</a></li>
+		<li><a href="https://mail.uoguelph.ca"><i className="icon-mail"></i>GryphMail</a></li>
+		<li><a href="https://courselink.uoguelph.ca"><i className="icon-pencil"></i>CourseLink</a></li>
+		<li><a href="https://webadvisor.uoguelph.ca"><i className="icon-webadvisor"></i>WebAdvisor</a></li>
+		<li><a href="https://gryphlife.uoguelph.ca"><i className="icon-gryphlife"></i>GryphLife</a></li>
+		<li><a href="https://www.lib.uoguelph.ca/"><i className="icon-library"></i>Library</a></li>
+		<li><a href="https://www.uoguelph.ca/campus/map/"><i className="icon-map"></i>Map</a></li>
+		<li><a href="https://www.uoguelph.ca/directory/"><i className="icon-directory"></i>Directory</a></li>
+	</ul>
+</div>
+)
+
+export default QuickLinks
