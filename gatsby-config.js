9b7fe4a6a633fbae6d19bc4e1ea2651343d31c2e
Tanya Qureshi <tqureshi@uoguelph.ca>
2020-10-05 14:35:03 -0400

    alphabetize plugin list and clarify dev comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,29 +4,41 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-let metaConfig = require('./config/sites/' + process.env._SITE + '.js');
-// uncomment the line below (and comment out the line above) for dev purposes:
-// let metaConfig = require('./config/sites/ugconthub.js');
+//let metaConfig = require('./config/sites/' + process.env._SITE + '.js');
+// For dev purposes, comment the line above and uncomment the line below:
+let metaConfig = require('./config/sites/ugconthub.js');
 
-if((metaConfig == null) || (metaConfig == undefined)) {
-  metaConfig['title'] = "Gatsby UG Starter Template";
-  metaConfig['description'] = "Description of the site";
-  metaConfig['author'] = "Author of site";
-  metaConfig['GAtrackingID'] = "";
-  metaConfig['IGuser'] = "";
+if ((metaConfig == null) || (metaConfig == undefined)) {
+	metaConfig['title'] = "Gatsby UG Starter Template";
+	metaConfig['description'] = "Description of the site";
+	metaConfig['author'] = "Author of site";
+	metaConfig['GAtrackingID'] = "";
+	metaConfig['IGuser'] = "";
 }
 
 module.exports = {
-  siteMetadata: {
-    title: metaConfig['title'],
-    description: metaConfig['description'],
-    author: metaConfig['author'],
-  },
-  plugins: [
+	siteMetadata: {
+	  title: metaConfig['title'],
+	  description: metaConfig['description'],
+	  author: metaConfig['author'],
+	},
+	plugins: [
+	{
+	  resolve: `gatsby-plugin-google-analytics`,
+	  options: {
+		trackingId: metaConfig['GAtrackingID'],
+	  },
+    },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+	  resolve: `gatsby-plugin-manifest`,
       options: {
-        trackingId: metaConfig['GAtrackingID'],
+        name: `Gatsby UG Starter Template`,
+        short_name: `UG starter`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#000000`,
+        display: `browser`,
+        icon: `src/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
@@ -44,42 +56,32 @@ module.exports = {
 	  },
 	},
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby UG Starter Template`,
-        short_name: `UG starter`,
-        start_url: `/`,
-        background_color: `#FFFFFF`,
-        theme_color: `#000000`,
-        display: `browser`,
-        icon: `src/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-       resolve: `gatsby-source-drupal`,
-       options: {
-         baseUrl: `https://api.` + process.env._GATSBY_SOURCE + process.env._SITE + `.` + process.env._ZONE + `/`,
-		 // uncomment the line below (and comment out the line above) for dev purposes:
-		 //baseUrl: `https://api.devugconthub.uoguelph.dev/`,
-         apiBase: `jsonapi`, // optional, defaults to `jsonapi`
-       },
-    },    
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
       },
-    },    
+    },	
+    {
+	  resolve: `gatsby-source-drupal`,
+	  options: {
+	   //baseUrl: `https://api.` + process.env._GATSBY_SOURCE + process.env._SITE + `.` + process.env._ZONE + `/`,
+	   // For dev purposes, comment the line above and uncomment the appropriate line below:
+	   //baseUrl: `https://api.devugconthub.uoguelph.dev/`,
+	   //baseUrl: `http://multidevname-bovey.pantheonsite.io/`,
+	    baseUrl: `http://video-bovey.pantheonsite.io/`,
+	    apiBase: `jsonapi`, // optional, defaults to `jsonapi`
+	  },
+    },
     {
       resolve: `gatsby-source-instagram`,
       options: {
         username: metaConfig['IGuser'],        
       },
-    },	
-  ],
-  mapping: {
-    "node__page.fields.alias": `PathAlias`,
-    "node__program.fields.alias": `PathAlias`,
-  },
+    },
+	],
+	mapping: {
+	  "node__page.fields.alias": `PathAlias`,
+	  "node__program.fields.alias": `PathAlias`,
+	},
 }
