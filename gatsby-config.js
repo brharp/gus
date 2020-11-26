3ad4dcbd6334999abd638dbcf8bba180bb4b8d87
M. Brent Harp <brharp@uoguelph.ca>
2020-11-26 11:03:51 -0500

    Hard code source for Az static web app test

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-let metaConfig = require('./config/sites/' + process.env._SITE + '.js');
+let metaConfig = require('./config/sites/ugconthub.js');
 
 if((metaConfig == null) || (metaConfig == undefined)) {
   metaConfig['title'] = "Gatsby UG Starter Template";
@@ -45,7 +45,7 @@ module.exports = {
     {
        resolve: `gatsby-source-drupal`,
        options: {
-         baseUrl: `https://api.` + process.env._GATSBY_SOURCE + process.env._SITE + `.` + process.env._ZONE + `/`,
+         baseUrl: `https://api.liveugconthub.uoguelph.dev/`,
          apiBase: `jsonapi`, // optional, defaults to `jsonapi`
        },
     },
